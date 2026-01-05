a217eb6288de2885435840c4ba880eda37c58c9f
shubham singh <student@gmail.com>
2026-01-05 10:09:23 +0530

    add

--- client/src/components/home/CallToAction.jsx
+++ client/src/components/home/CallToAction.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 const CallToAction = () => {
     const {user} = useSelector(state => state.auth)
